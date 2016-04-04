3552a707a2ebd19ffbb2a9c4016d2bdf414d2157
Groboclown <matt@groboclown.net>
2016-04-04 18:59:40 -0500

    Fix the changelist name/comment concatenation issue (#91)

--- plugin/src/net/groboclown/idea/p4ic/v2/server/util/ChangelistDescriptionGenerator.java
+++ plugin/src/net/groboclown/idea/p4ic/v2/server/util/ChangelistDescriptionGenerator.java
@@ -14,6 +14,7 @@
 package net.groboclown.idea.p4ic.v2.server.util;
 
 import com.intellij.openapi.vcs.changes.ChangeList;
+import net.groboclown.idea.p4ic.P4Bundle;
 import org.jetbrains.annotations.NotNull;
 
 public class ChangelistDescriptionGenerator {
@@ -22,28 +23,12 @@ public class ChangelistDescriptionGenerator {
         // the name and the description for the comment.  Instead, this should match the "submit"
         // functionality and only use the comment.
 
-        StringBuilder sb = new StringBuilder();
-        if (ideaChange.getName().length() > 0) {
-            sb.append(ideaChange.getName());
-        }
         if (ideaChange.getComment() != null && ideaChange.getComment().length() > 0) {
-            String comment = ideaChange.getComment();
-            if (sb.length() > 0) {
-
-
-                String head = sb.toString();
-                if (head.endsWith("...")) {
-                    head = head.substring(0, head.length() - 3);
-                }
-                if (head.trim().equalsIgnoreCase(comment)) {
-                    // Just use the comment, not the name + comment.
-                    sb.setLength(0);
-                } else {
-                    sb.append("\n\n");
-                }
-            }
-            sb.append(ideaChange.getComment());
+            return ideaChange.getComment();
         }
-        return sb.toString();
+        if (ideaChange.getName().length() > 0) {
+            return ideaChange.getName();
+        }
+        return P4Bundle.message("changelist.no-description");
     }
 }

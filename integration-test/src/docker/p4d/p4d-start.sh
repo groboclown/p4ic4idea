f7d79482e99fdb84c96a1b2106bf8e9a4a8b48aa
Groboclown <matt@groboclown.net>
2018-11-09 11:52:00 -0600

    Fix up the daemon startup so it accounts for persistent volumes, and have the swarm server run apache in non-debug mode.

--- integration-test/src/docker/p4d/p4d-start.sh
+++ integration-test/src/docker/p4d/p4d-start.sh
@@ -6,12 +6,13 @@ P4D=/usr/local/sbin/p4d.bin
 
 if [ ! -d "$P4ROOT" ]; then
     mkdir -p "$P4ROOT" || exit 1
-    if [ -f /opt/sampledepot.tar.gz ]; then
-        ( cd /tmp && tar zxf /opt/sampledepot.tar.gz ) || exit 1
-        mv /tmp/PerforceSample/* "$P4ROOT" || exit 1
-        $P4D -jr "$P4ROOT"/checkpoint || exit 1
-        $P4D -xu || exit 1
-    fi
+fi
+if [ ! -d "$P4ROOT/HR" -a -f /opt/sampledepot.tar.gz ]; then
+    ( cd /tmp && tar zxf /opt/sampledepot.tar.gz ) || exit 1
+    rm -r "$P4ROOT"/* || exit 1
+    mv /tmp/PerforceSample/* "$P4ROOT" || exit 1
+    $P4D -jr "$P4ROOT"/checkpoint || exit 1
+    $P4D -xu || exit 1
 fi
 
 if [ "$P4PORT" = "ssl:1666" ]; then
--- integration-test/src/docker/swarm/start-apache.sh
+++ integration-test/src/docker/swarm/start-apache.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+test -f /opt/swarm/data/p4trust && rm /opt/swarm/data/p4trust
 test -f /run/apache2/httpd.pid && rm /run/apache2/httpd.pid
 cp /opt/config.php /opt/swarm/data
 chmod -R g+rw /opt/swarm/data
 chmod -R a+rw /opt/swarm/data
-exec /usr/sbin/httpd -X
+
+# Debug mode:
+# /usr/sbin/httpd -X
+
+/usr/sbin/httpd || exit 1
+
+# Cron job to run workers.
+while [ 1 = 1 ]; do
+    curl --silent --insecure --output /dev/null --max-time 5 "http://localhost/queue/worker"
+    sleep 5
+done

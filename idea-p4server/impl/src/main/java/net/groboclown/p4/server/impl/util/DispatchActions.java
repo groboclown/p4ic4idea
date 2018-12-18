56a69daf1522e448e681d9bcb95178d344a0fc04
Groboclown <matt@groboclown.net>
2018-12-18 12:15:40 -0600

    Clean up unused methods and classes.

--- idea-p4server/impl/src/main/java/net/groboclown/p4/server/impl/util/DispatchActions.java
+++ idea-p4server/impl/src/main/java/net/groboclown/p4/server/impl/util/DispatchActions.java
@@ -16,13 +16,7 @@ package net.groboclown.p4.server.impl.util;
 
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.util.Computable;
-import com.intellij.openapi.vcs.FilePath;
-import com.intellij.openapi.vfs.VirtualFile;
-import net.groboclown.p4.server.todo.FileOperationErrorManager;
 import org.jetbrains.annotations.NotNull;
-
-import java.io.IOException;
 
 /**
  * Performs operations that, if run within the dispatch thread, require special handling.
@@ -41,72 +35,5 @@ public class DispatchActions {
         } else {
             run.run();
         }
-    }
-
-    public static <T> T writeAction(@NotNull Computable<T> comp) {
-        final Application app = ApplicationManager.getApplication();
-        if (app.isDispatchThread() && !app.isWriteAccessAllowed()) {
-            return app.runWriteAction(comp);
-        } else {
-            return comp.compute();
-        }
-    }
-
-    public static void readAction(@NotNull Runnable run) {
-        final Application app = ApplicationManager.getApplication();
-        if (app.isDispatchThread() && !app.isReadAccessAllowed()) {
-            app.runReadAction(run);
-        } else {
-            run.run();
-        }
-    }
-
-    public static <T> T readAction(@NotNull Computable<T> comp) {
-        final Application app = ApplicationManager.getApplication();
-        if (app.isDispatchThread() && !app.isReadAccessAllowed()) {
-            return app.runReadAction(comp);
-        } else {
-            return comp.compute();
-        }
-    }
-
-    public static boolean setWritable(@NotNull final FilePath fp, @NotNull final FileOperationErrorManager errMgr) {
-        if (fp.getVirtualFile() != null) {
-            return setWritable(fp.getVirtualFile(), errMgr);
-        }
-        return writeAction(() -> {
-            boolean ret = fp.getIOFile().setWritable(true);
-            if (! ret) {
-                errMgr.warnCouldNotMakeWritable(fp);
-            }
-            return ret;
-        });
-    }
-
-    public static boolean setWritable(@NotNull final VirtualFile vf, @NotNull final FileOperationErrorManager errMgr) {
-        if (! vf.isInLocalFileSystem()) {
-            return false;
-        }
-        return writeAction(() -> {
-            try {
-                vf.setWritable(true);
-                return true;
-            } catch (IOException e) {
-                errMgr.warnCouldNotMakeWritable(vf);
-                return false;
-            }
-        });
-    }
-
-    public static void setWritable(@NotNull final VirtualFile[] files, @NotNull final FileOperationErrorManager errMgr) {
-        writeAction(() -> {
-            for (VirtualFile vf: files) {
-                try {
-                    vf.setWritable(true);
-                } catch (IOException e) {
-                    errMgr.warnCouldNotMakeWritable(vf);
-                }
-            }
-        });
     }
 }
